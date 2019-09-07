a5d818997d35f593678f5200fb437916b03f1b6b
haksh <akshay.hariharan@mail.utoronto.ca>
2019-09-07 21:51:00 +0200

    Added non working functionality for beta.
    Issue: NaNs. NaNs EVERYWHERE!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
+import pandas as pd
 import sklearn as sk
-from pandas_datareader import data
 import datetime
 import json, random
 import yfinance as yf
@@ -19,6 +19,7 @@ def generate_random_stocks(n):
     return random_stocks
 
 def get_date_data_available_from(random_stocks):
+    random_stocks.append('SPY')
     oldest_date = datetime.date.min
     for stock in random_stocks:
         tcker = yf.Ticker(stock)
@@ -30,14 +31,27 @@ def get_date_data_available_from(random_stocks):
     print('Stocks all exist from: ' + str(oldest_date))
     return oldest_date
 
-def get_price_data_from_date(random_stocks, oldest_date, histories):
+def get_price_data_from_date(oldest_date, histories):
     data = {}
-    for stock in random_stocks:
+    for stock in histories:
         history = histories[stock]
         subset = history[history.index > oldest_date]
         data[stock] = subset
     return data
 
+def beta(index, stock):
+    index_closing_pct_chg = index['Close'].pct_change()
+    stock_closing_pct_chg = stock['Close'].pct_change()
+    df = pd.DataFrame(stock_closing_pct_chg,index_closing_pct_chg)
+    return df.cov()
+
+
+histories = {}
+random_stocks = generate_random_stocks(2)
+date = get_date_data_available_from(random_stocks)
+data = get_price_data_from_date(date, histories)
+for stock in data:
+    print(beta(data['SPY'], data[stock]))
 
 #print(history[history.columns.tolist()[1]].tolist())
 #data = yf.download('uber', '', '2019-01-01')
