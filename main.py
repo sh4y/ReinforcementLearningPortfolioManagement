31238e70804595b2e7b96d8b43c074b437d5f49b
sh4y <hackshay96@gmail.com>
2019-09-18 13:12:53 +0200

    Fixed beta calculations

--- main.py
+++ main.py
@@ -6,58 +6,67 @@ import json, random
 import yfinance as yf
 yf.pdr_override()
 
+with open('constituents_json.json', 'r') as f:
+    sp500 = json.loads(f.read());
 histories = {}
 
 def store_stock_history(stock_name, data):
     histories[stock_name] = data
 
 def generate_random_stocks(n):
-    with open('constituents_json.json', 'r') as f:
-        sp500 = json.loads(f.read());
-
     random_stocks = [random.choice(sp500)['Symbol'] for x in range(n)]
     #append spy as a "random" stock for future use
     random_stocks.append('SPY')
     return random_stocks
 
-def get_date_data_available_from(random_stocks):
+def get_date_data_available_from(stocks):
     oldest_date = datetime.date.min
-    for stock in random_stocks:
-        tcker = yf.Ticker(stock)
-        history = tcker.history('max')
-        store_stock_history(stock, history)
-        beginning = history.index[0]
-        if beginning > oldest_date:
-            oldest_date = beginning
+    for stock in stocks:
+        try:
+            tcker = yf.Ticker(stock)
+            history = tcker.history('max')
+            store_stock_history(stock, history)
+            beginning = history.index[0]
+            if beginning > oldest_date:
+                oldest_date = beginning
+        except ValueError:
+            print('Value Error found for stock ', stock)
+            print('Removing stock ' + str(stock) + ' from list, adding a new one.')
+            stocks.remove(stock)
+            stocks.append(random.choice(sp500)['Symbol'])
     print('Stocks all exist from: ' + str(oldest_date))
     return oldest_date
+
 
 def get_price_data_from_date(oldest_date, histories):
     data = {}
     for stock in histories:
         history = histories[stock]
-        subset = np.array(history[history.index > oldest_date])
+        subset = history[history.index > oldest_date]
         data[stock] = subset
     return data
+
+def pct_change(a):
+    a = np.array(a)
+    return np.diff(a) / a[:, 1:]
 
 def beta(index, stock):
     index_closing_pct_chg = index['Close'].pct_change()
     stock_closing_pct_chg = stock['Close'].pct_change()
-    df = pd.DataFrame(stock_closing_pct_chg,index_closing_pct_chg)
+    d = {'Index Changes': index_closing_pct_chg, 'Stock Change': stock_closing_pct_chg}
+    df = pd.DataFrame(d)
     return df.cov()
 
+def calculate_covariances(stocks, prices):
+    covars = {}
+    for stock in stocks:
+        # calculate covar between stock and index
+        if stock != 'SPY':
+            cov = beta(prices['SPY'], prices[stock])
+            covars[stock] = cov
+    return covars
 
-histories = {}
-random_stocks = generate_random_stocks(2)
-date = get_date_data_available_from(random_stocks)
-data = get_price_data_from_date(date, histories)
-for stock in data:
-    print(beta(data['SPY'], data[stock]))
-
-#print(history[history.columns.tolist()[1]].tolist())
-#data = yf.download('uber', '', '2019-01-01')
-
-
-stocks = generate_random_stocks(20)
+stocks = generate_random_stocks(3)
 oldest_date = get_date_data_available_from(stocks)
-prices = get_price_data_from_date(stocks, oldest_date, histories)
+prices = get_price_data_from_date(oldest_date, histories)
+betas = calculate_covariances(stocks, prices)
